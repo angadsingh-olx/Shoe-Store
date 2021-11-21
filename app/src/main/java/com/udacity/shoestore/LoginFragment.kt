44f5d9a09495d676a9d90f909dd369bf9e7566b9
Angad Singh <angad.singh@olx.com>
2021-11-21 14:29:34 +0530

    Implemented login screen

--- app/src/main/java/com/udacity/shoestore/LoginFragment.kt
+++ app/src/main/java/com/udacity/shoestore/LoginFragment.kt
@@ -6,9 +6,10 @@ import android.view.View
 import android.view.ViewGroup
 import androidx.databinding.DataBindingUtil
 import androidx.fragment.app.Fragment
+import androidx.navigation.findNavController
 import com.udacity.shoestore.databinding.FragmentLoginBinding
 
-class LoginFragment: Fragment() {
+class LoginFragment: Fragment(), View.OnClickListener {
 
     private lateinit var viewBinding: FragmentLoginBinding
 
@@ -18,6 +19,16 @@ class LoginFragment: Fragment() {
         savedInstanceState: Bundle?
     ): View {
         viewBinding = DataBindingUtil.inflate(layoutInflater, R.layout.fragment_login, container, false)
+        viewBinding.actionSignIn.setOnClickListener(this)
+        viewBinding.actionSignUp.setOnClickListener(this)
         return viewBinding.root
+    }
+
+    override fun onClick(v: View?) {
+        when(v?.id) {
+            R.id.actionSignIn, R.id.actionSignUp -> {
+                viewBinding.root.findNavController().navigate(LoginFragmentDirections.actionLoginFragmentToOnboardingFragment())
+            }
+        }
     }
 }
