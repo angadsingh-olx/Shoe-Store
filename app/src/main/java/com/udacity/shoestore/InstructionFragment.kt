e4de4b93459b2567e596bdbfaf8dfd3a606bd85a
Angad Singh <angad.singh@olx.com>
2021-11-21 16:44:55 +0530

    Implemented onboarding fragment

--- app/src/main/java/com/udacity/shoestore/InstructionFragment.kt
+++ app/src/main/java/com/udacity/shoestore/InstructionFragment.kt
@@ -6,24 +6,19 @@ import android.view.View
 import android.view.ViewGroup
 import androidx.databinding.DataBindingUtil
 import androidx.fragment.app.Fragment
-import androidx.navigation.findNavController
+import com.udacity.shoestore.databinding.FragmentInstructionBinding
 import com.udacity.shoestore.databinding.FragmentOnboardingBinding
 
-class OnboardingFragment: Fragment() {
+class InstructionFragment: Fragment() {
 
-    private lateinit var viewBinding: FragmentOnboardingBinding
+    private lateinit var viewBinding: FragmentInstructionBinding
 
     override fun onCreateView(
         inflater: LayoutInflater,
         container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View {
-        viewBinding = DataBindingUtil.inflate(layoutInflater, R.layout.fragment_onboarding, container, false)
-
-        viewBinding.actionNext.setOnClickListener{
-            viewBinding.root.findNavController().navigate(OnboardingFragmentDirections.actionOnboardingFragmentToInstructionFragment())
-        }
-
+        viewBinding = DataBindingUtil.inflate(layoutInflater, R.layout.fragment_instruction, container, false)
         return viewBinding.root
     }
 }
